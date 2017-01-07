d9c2804ad36be50fcb6420b78134b480f68f7e2c
Kai Brüning <kai@granus.net>
2017-01-07 15:14:33 +0100

    Protokollausgabe verbessert.

--- VereinsFinesse/MainController.py
+++ VereinsFinesse/MainController.py
@@ -38,10 +38,6 @@ class MainController:
         self.fehlerhafte_vf_buchungen = []
         self.fehlerhafte_finesse_buchungen = []
 
-    @property
-    def has_fehlerhafte_buchungen(self):
-        return len(self.fehlerhafte_vf_buchungen) > 0 or len(self.fehlerhafte_finesse_buchungen) > 0
-
     def run(self):
         # argparse exits on error after printing a message - no need to modify this.
         self.parse_args()
@@ -49,6 +45,8 @@ class MainController:
         self.open_protokoll_file()
 
         try:
+            self.delete_old_output_files()
+
             self.open_config()
 
             path = self.vf_export_path()
@@ -57,17 +55,17 @@ class MainController:
             path = self.finesse_export_path()
             if path:
                 self.import_finesse(path)
-            self.raise_if_pending_errors()
+            #self.raise_if_pending_errors()
 
             self.connectImportedVFBuchungen()
             self.connectImportedFinesseBuchungen()
-            self.raise_if_pending_errors()
+            #self.raise_if_pending_errors()
 
             self.entferne_stornierte_finesse_buchungen()
 
             finesse_export_list = self.finesseBuchungenForExportToVF()
             vf_exportList = self.vfBuchungenForExportToFinesse()
-            self.raise_if_pending_errors()
+            #self.raise_if_pending_errors()
 
             if len(finesse_export_list) > 0:
                 f = open(vf_import_file_name, 'wb')
@@ -78,14 +76,23 @@ class MainController:
                 self.exportVFBuchungenToFinesse(vf_exportList, f)
 
         except StopRun:
-            self.report_errors()
-            self.protokoll_stream.write(u"Abbruch nach Fehlern.")
+            self.protokoll_stream.writelines([u"Abbruch nach Fehlern.", os.linesep])
 
         except:
-            self.protokoll_stream.write(u"Abbruch wegen unerwarteten Fehlers: {0}".format(sys.exc_info()[0]))
+            self.protokoll_stream.writelines([u"Abbruch wegen unerwarteten Fehlers: {0}".format(sys.exc_info()), os.linesep])
 
         else:
-            self.protokoll_stream.write(u"Datenaustausch erfolgreich beendet.")
+            if self.has_fehlerhafte_buchungen:
+                self.protokoll_stream.writelines([u"Datenaustausch beendet.", os.linesep])
+                if len(self.fehlerhafte_vf_buchungen) > 0:
+                    self.protokoll_stream.writelines([u"{0} Buchungen aus dem Vereinsflieger konnten nicht bearbeitet werden.".format(len(self.fehlerhafte_vf_buchungen)), os.linesep])
+                if len(self.fehlerhafte_finesse_buchungen) > 0:
+                    self.protokoll_stream.writelines([u"{0} Buchungen aus Finesse konnten nicht bearbeitet werden.".format(len(self.fehlerhafte_finesse_buchungen)), os.linesep])
+                self.protokoll_stream.writelines([u"Siehe „{0}“ für Details.".format(fehler_file_name), os.linesep])
+            else:
+                self.protokoll_stream.writelines([u"Datenaustausch ohne Fehler beendet.", os.linesep])
+
+            self.report_fehlerhafte_buchungen()
 
     def parse_args(self):
         parser = argparse.ArgumentParser()
@@ -103,6 +110,18 @@ class MainController:
 
         self.protokoll_stream = codecs.getwriter("utf-8")(f)
 
+    def delete_old_output_files(self):
+        # Make sure we leave no old output behind.
+        # os.remove() throws if the file does not exist, just continue in this case.
+        try:
+            os.remove(vf_import_file_name)
+        except:
+            pass
+        try:
+            os.remove(finesse_import_file_name)
+        except:
+            pass
+
     def open_config(self):
         # Zur Zeit muss der Konfigurationspfad als Parameter angegeben werden.
         # TODO: implement default behavior
@@ -115,7 +134,7 @@ class MainController:
             config_dict = yaml.load(stream)
             self.konfiguration = Configuration.Konfiguration(config_dict)
         except yaml.parser.ParserError as error:
-            self.protokoll_stream.write(u"Fehler beim Lesen der Konfiguration von „{0}“: {1}".format(stream.name, error))
+            self.protokoll_stream.writelines([u"Fehler beim Lesen der Konfiguration von „{0}“: {1}".format(stream.name, error), os.linesep])
             raise StopRun()
 
     def is_buchung_exported_to_finesse(self, buchung):
@@ -152,7 +171,7 @@ class MainController:
 
         for row_dict in reader:
             if u'<ÜBERHANG>' in row_dict:
-                self.protokoll_stream.write(u'Quellzeile {0} in {1} enthält unerwartete Daten {2}.'.format(reader.line_num, path, row_dict[u'<ÜBERHANG>']))
+                self.protokoll_stream.writelines([u'Quellzeile {0} in {1} enthält unerwartete Daten {2}.'.format(reader.line_num, path, row_dict[u'<ÜBERHANG>']), os.linesep])
                 raise StopRun()
 
             b = VF_Buchung.VF_Buchung(self.konfiguration)
@@ -210,7 +229,7 @@ class MainController:
 
         for row_dict in reader:
             if u'<ÜBERHANG>' in row_dict:
-                self.protokoll_stream.write(u'Quellzeile {0} in {1} enthält unerwartete Daten {2}.'.format(reader.line_num, path, row_dict[u'<ÜBERHANG>']))
+                self.protokoll_stream.writelines([u'Quellzeile {0} in {1} enthält unerwartete Daten {2}.'.format(reader.line_num, path, row_dict[u'<ÜBERHANG>']), os.linesep])
                 raise StopRun()
 
             b = Finesse_Buchung.Finesse_Buchung(self.konfiguration)
@@ -259,27 +278,35 @@ class MainController:
 
     def connectImportedVFBuchungen(self):
         for vf_buchung in self.vf_buchungenImportedFromFinesse.itervalues():
-            fehler_beschreibung = None
+            vf_fehler_beschreibung = None
+            finesse_fehler_beschreibung = None
+            original_finesse_buchung = None
+
             finesse_journalnummer = vf_buchung.finesse_journalnummer
             if not finesse_journalnummer in self.finesse_buchungen_for_export_to_vf_by_finesse_fournal_nr:
-                fehler_beschreibung = u'Originale Finesse-Buchung (Dialog: {0}) nicht gefunden'.format(finesse_journalnummer)
+                vf_fehler_beschreibung = u'Originale Finesse-Buchung (Dialog: {0}) nicht gefunden'.format(finesse_journalnummer)
             else:
                 original_finesse_buchung = self.finesse_buchungen_for_export_to_vf_by_finesse_fournal_nr[finesse_journalnummer]
                 if original_finesse_buchung.kopierte_vf_buchung:
-                    fehler_beschreibung = u'Mehrere VF-Buchungen zu einer Journalnummer in Finesse (Dialog: {0})'.format(finesse_journalnummer)
+                    vf_fehler_beschreibung = u'Mehrere VF-Buchungen zu einer Journalnummer in Finesse (Dialog: {0})'.format(finesse_journalnummer)
+                    finesse_fehler_beschreibung = u'Mehrere VF-Buchungen zu dieser Finesse-Buchung gefunden'
                 else:
                     # Buchungen in Finesse dürfen sich zwischen Synchronisierungen nicht ändern, und die Kopie
                     # im VF darf auch nicht geändert werden.
                     if not vf_buchung.validate_for_original_finesse_buchung(original_finesse_buchung):
                         vf_buchung.validate_for_original_finesse_buchung(original_finesse_buchung)  # repeated for debugging
-                        fehler_beschreibung = u'Importierte VF-Buchung weicht von originaler Finesse-Buchung (Dialog: {0}) ab'.format(finesse_journalnummer)
+                        vf_fehler_beschreibung = u'Importierte VF-Buchung weicht von originaler Finesse-Buchung (Dialog: {0}) ab'.format(finesse_journalnummer)
+                        finesse_fehler_beschreibung = u'Originaler Finesse-Buchung weicht von importierte VF-Buchung ab'
                     else:
                         vf_buchung.original_finesse_buchung = original_finesse_buchung
                         original_finesse_buchung.kopierte_vf_buchung = vf_buchung
 
-            if fehler_beschreibung:
-                vf_buchung.fehler_beschreibung = fehler_beschreibung
+            if vf_fehler_beschreibung:
+                vf_buchung.fehler_beschreibung = vf_fehler_beschreibung
                 self.fehlerhafte_vf_buchungen.append(vf_buchung)
+            if finesse_fehler_beschreibung:
+                original_finesse_buchung.fehler_beschreibung = finesse_fehler_beschreibung
+                self.fehlerhafte_finesse_buchungen.append(original_finesse_buchung)
 
     def finesseBuchungenForExportToVF(self):
         """
@@ -287,7 +314,7 @@ class MainController:
         """
         result = []
         for finesse_buchung in self.finesse_buchungen_for_export_to_vf_by_finesse_fournal_nr.itervalues():
-            if not finesse_buchung.kopierte_vf_buchung:
+            if not finesse_buchung.kopierte_vf_buchung and not finesse_buchung.fehler_beschreibung:
                 vf_buchung = finesse_buchung.vf_buchung_for_export()
                 if vf_buchung:
                     result.append(vf_buchung)
@@ -346,39 +373,41 @@ class MainController:
             finesse_buchungen = vf_buchung.finesse_buchungen_from_vf_buchung()
             if finesse_buchungen != None:
                 result += finesse_buchungen
-
-                # Finesse protestiert bei Buchung mit Betrag 0, was irgendwie verständlich ist.
-                #if finesseBuchung.betrag_soll != Decimal(0) and finesseBuchung.betrag_haben != Decimal(0):
-                 #   result.append(finesseBuchung)
             else:
                 self.fehlerhafte_vf_buchungen.append(vf_buchung)
 
-        if len(self.fehlerhafte_vf_buchungen) > 0:
-            raise StopRun()
+        #if len(self.fehlerhafte_vf_buchungen) > 0:
+        #    raise StopRun()
 
         return result
 
-    def raise_if_pending_errors(self):
-       if len(self.fehlerhafte_vf_buchungen) > 0 or len(self.fehlerhafte_finesse_buchungen) > 0:
-            raise StopRun()
+    @property
+    def has_fehlerhafte_buchungen(self):
+        return len(self.fehlerhafte_vf_buchungen) > 0 or len(self.fehlerhafte_finesse_buchungen) > 0
 
-    def report_errors(self):
-        f = open(fehler_file_name, 'wb')
+    def report_fehlerhafte_buchungen(self):
+        if self.has_fehlerhafte_buchungen:
+            f = open(fehler_file_name, 'wb')
 
-        # Mark the file as using utf-8
-        bom = bytearray([239, 187, 191])    # the utf-8 byte order mark
-        f.write(bom)
-        f_utf8 = codecs.getwriter("utf-8")(f)
+            # Mark the file as using utf-8
+            bom = bytearray([239, 187, 191])    # the utf-8 byte order mark
+            f.write(bom)
+            f_utf8 = codecs.getwriter("utf-8")(f)
 
-        if len(self.fehlerhafte_vf_buchungen) > 0:
-            f_utf8.writelines([u"Folgende Buchungen aus dem Vereinsflieger können nicht verarbeitet werden:", os.linesep])
-            self.write_fehlerhafte_buchungen(self.fehlerhafte_vf_buchungen, self.vf_export_fieldnames, f)
-            f_utf8.writelines([os.linesep])
+            if len(self.fehlerhafte_vf_buchungen) > 0:
+                f_utf8.writelines([u"Folgende Buchungen aus dem Vereinsflieger können nicht verarbeitet werden:", os.linesep])
+                self.write_fehlerhafte_buchungen(self.fehlerhafte_vf_buchungen, self.vf_export_fieldnames, f)
+                f_utf8.writelines([os.linesep])
 
-        if len(self.fehlerhafte_finesse_buchungen) > 0:
-            f_utf8.write(u"Folgende Buchungen aus Finesse können nicht verarbeitet werden:")
-            self.write_fehlerhafte_buchungen(self.fehlerhafte_finesse_buchungen, self.finesse_export_fieldnames, f)
-            f_utf8.write(u"")
+            if len(self.fehlerhafte_finesse_buchungen) > 0:
+                f_utf8.write(u"Folgende Buchungen aus Finesse können nicht verarbeitet werden:")
+                self.write_fehlerhafte_buchungen(self.fehlerhafte_finesse_buchungen, self.finesse_export_fieldnames, f)
+                f_utf8.write(u"")
+        else:
+            try:
+                os.remove(fehler_file_name)
+            except:
+                pass
 
     def write_fehlerhafte_buchungen(self, buchungen, fieldnames, filehandle):
         fieldnames.insert(0, u'Fehler')
